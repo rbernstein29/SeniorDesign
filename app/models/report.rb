44cfa62b966668f099a84761b7c33cd7c75ef1a7
EmmanuelMar04 <emartinez5@pride.hofstra.edu>
2026-03-10 16:47:56 -0400

    sync report to account

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,6 @@
 class Report < ApplicationRecord
-  self.table_name = "vuln_scanner.reports" 
+  self.table_name = "vuln_scanner.reports"
+
+  belongs_to :user
+  belongs_to :scan, optional: true
 end
