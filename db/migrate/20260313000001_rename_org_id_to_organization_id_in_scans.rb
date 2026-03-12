4d75aa6fa11e4eec45872fcb49be15acec6db18c
Trevor Conley <trevorconley03@gmail.com>
2026-03-12 18:05:44 -0400

    Make org_id migration safe when column already renamed

--- db/migrate/20260313000001_rename_org_id_to_organization_id_in_scans.rb
+++ db/migrate/20260313000001_rename_org_id_to_organization_id_in_scans.rb
@@ -1,5 +1,5 @@
 class RenameOrgIdToOrganizationIdInScans < ActiveRecord::Migration[7.2]
   def change
-    rename_column :scans, :org_id, :organization_id
+    rename_column :scans, :org_id, :organization_id if column_exists?(:scans, :org_id)
   end
 end
